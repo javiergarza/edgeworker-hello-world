40ba3d26258133a82cdbb4371c8efb441c4ba76a
Javier Garza <7927364+javiergarza@users.noreply.github.com>
2020-09-11 00:40:32 -0700

    adding deployed by text

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 export function onClientRequest(request) {
   request.respondWith(
       200, {},
-      '<html><body><h1>Hello World From Akamai EdgeWorkers</h1></body></html>');
+      '<html><body><h1>Hello World From Akamai EdgeWorkers. Deployed by a GitHub action</h1></body></html>');
 }
 
 export function onClientResponse(request, response) {
